9ed23bb2cdeaae26f8c8a9e05081e625b9c27d1d
1095071913 <1095071913@qq.com>
2022-09-01 23:28:21 +0800

    init

--- jiumao-saas-service-user/src/main/java/com/jiumao/user/user/api/SysUserService.java
+++
@@ -1,8 +0,0 @@
-package com.jiumao.user.user.api;
-
-import com.github.yulichang.base.MPJBaseService;
-import com.jiumao.user.user.domain.SysUserDo;
-
-public interface SysUserService extends MPJBaseService<SysUserDo> {
-
-}
--- jiumao-saas-service-user/src/main/java/com/jiumao/user/user/mapper/SysUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.jiumao.user.user.mapper;
-
-import com.jiumao.user.user.domain.SysUserDo;
-import com.github.yulichang.base.MPJBaseMapper;
-
-public interface SysUserMapper extends MPJBaseMapper<SysUserDo> {
-
-
-}
